3a91447e5a81c67ea719bcb8f2b13f7a70fdfee6
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2023-01-12 15:41:29 +0000

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+test('Test header renders with correct text', () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe('Testing Playground')
+});
+
+
+test('Test for the button', () => {
+  render(<App />)
+  const colorBtn = screen.getByRole('button')
+
+  userEvent.click(colorBtn)
+
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'blue' })
+  expect(colorBtn.textContent).toBe('Change button color to green')
+})
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+  const checkbox = screen.getByRole("checkbox")
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeDisabled()
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeEnabled()
+})
